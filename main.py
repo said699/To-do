45bc4890fe5c1109a9be21ad59a374eb6d5ceaad
said <saidergashevtashkent@gmail.com>
2025-12-02 09:07:24 +0500

    Создана таблица и пустой шаблон

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from flask import Flask, render_template
-import psycopg2, os
+import os, datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'efjkt49u34923c23ruc3i34^£$B^£$%2c4£"4c!"445£$45vb4n543$52£c"£$23%!'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['MAX_CONTENT_LENGTH'] = 5*1024*1024
 
-@app.route('/')
-def main():
-    return render_template('main.html')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_INFO')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+print("DEBUG:", os.environ.get("DATABASE_INFO"))
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(300), nullable=False)
+    date_for_doing = db.Column(db.DateTime, nullable=False)
+    date_of_added = db.Column(db.DateTime, default=datetime.datetime.now(datetime.UTC))
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    psw = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
